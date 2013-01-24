3f16ba4fb162d3fd3c942900db57eafcde4421e4
Cheton Wu <cheton_wu@trend.com.tw>
2013-01-24 02:59:43 -0800

    Upgrade Grunt from 0.3 to 0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,22 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: '<json:package.json>',
+        pkg: grunt.file.readJSON('package.json'),
         meta: {
             banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
         },
         // Runs your QUnit tests
         qunit: {
-            files: ['test/**/*.html']
+            files: [
+                'test/**/*.html'
+            ]
         }
     });
 
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
     // Default tasks
-    grunt.registerTask('default', 'qunit');
+    grunt.registerTask('default', ['qunit']);
 
 };
