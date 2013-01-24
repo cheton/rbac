18512c5c7a1fe98ea8688d2b08063feb2ce0ef88
Cheton Wu <cheton_wu@trend.com.tw>
2013-01-24 03:07:36 -0800

    Added grunt-contrib-jshint plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,36 @@ module.exports = function(grunt) {
         meta: {
             banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
         },
-        // Runs your QUnit tests
+        jshint: {
+            options: {
+                es5: true, // Tolerate ES5 syntax
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    __dirname: true,
+                    process: true,
+                    exports: true,
+                    module: true,
+                    console: true,
+                    define: true,
+                    jQuery: true,
+                    '$': true
+                }
+            },
+            all: [
+                'Gruntfile.js',
+                'rbac.js'
+            ]
+        },
         qunit: {
             files: [
                 'test/**/*.html'
@@ -16,9 +45,10 @@ module.exports = function(grunt) {
     });
 
     // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Default tasks
-    grunt.registerTask('default', ['qunit']);
+    grunt.registerTask('default', ['jshint', 'qunit']);
 
 };
